84fbdc7744fe50518eb54fd2d95c8ede74da876d
Asad Saeeduddin <masaeedu@gmail.com>
2015-12-01 22:46:14 -0500

    Fixes for PRM

--- lab3/src/prm.cpp
+++ lab3/src/prm.cpp
@@ -1,17 +1,22 @@
 #include <Eigen/Dense>
+#include <tuple>
+#include <vector>
+#include <nav_msgs/OccupancyGrid.h>
 
-using std;
+using namespace std;
 
 typedef tuple<int, int> indices;
 typedef tuple<double, double> coord;
-map<int, map<int, double>> edgeset;
+typedef map<int, map<int, double>> edgeset;
+
+short sgn(int x) { return x >= 0 ? 1 : -1; }
 
 void bresenham(indices idx1, indices idx2, vector<int>& x, vector<int>& y) {
-	// Unpack indices
-	int x0, y0, x1, y1;
-	tie(x0, y0) = idx1;
-	tie(x1, y1) = idx2;
-	
+    // Unpack indices
+    int x0, y0, x1, y1;
+    tie(x0, y0) = idx1;
+    tie(x1, y1) = idx2;
+    
     int dx = abs(x1 - x0);
     int dy = abs(y1 - y0);
     int dx2 = x1 - x0;
@@ -52,170 +57,173 @@ void bresenham(indices idx1, indices idx2, vector<int>& x, vector<int>& y) {
     }
 }
 
-vector<int> find_nearest_neighbors(coord point, vector<coord> candidates, int k) {
-	// Create result vector
-	vector<int> idx(candidates.size()); 
-	iota(idx.begin(), idx.end(), 0);
-	
-	partial_sort(idx.begin(), idx.begin() + k, idx.end(), [=](coord a, coord b) {
-		return get_distance(point, candidates[a]) - get_distance(point, candidates[b]);
-	});
-	return vector<int>(idx.begin(), idx.begin() + k);
-}
-
 double get_distance(coord a, coord b) {
-	return sqrt(pow(get<0>(a) - get<0>(b), 2) + pow(get<1>(a) - get<1>(b), 2));
+    return sqrt(pow(get<0>(a) - get<0>(b), 2) + pow(get<1>(a) - get<1>(b), 2));
 }
 
-coords sample_point(gazebo_msgs::OccupancyGrid grid) {
-	thread_local static mt19937 rng(std::random_device{}());
-	thread local static uniform_real_distribution<double> dist(0.0, 1.0);
-		
-	double mapWidth = grid.info.resolution * grid.info.width;
-	double mapHeight = grid.info.resolution * grid.info.height;
-	
-	return {
-		dist(rng) * mapWidth - grid.info.origin.position.x, 
-		dist(rng) * mapHeight - grid.info.origin.position.y
-	};
+vector<int> find_nearest_neighbors(coord point, vector<coord> candidates, int k) {
+    // Create result vector
+    vector<int> idx(candidates.size()); 
+    iota(idx.begin(), idx.end(), 0);
+    
+    partial_sort(idx.begin(), idx.begin() + k, idx.end(), [=](int a, int b) {
+        return get_distance(point, candidates[a]) - get_distance(point, candidates[b]);
+    });
+    return vector<int>(idx.begin(), idx.begin() + k);
 }
 
-boolean is_connection_valid(coord start, coord finish, gazebo_msgs::OccupancyGrid grid) {
-	auto idx1 = bresenham(get_indices(start))
+coord sample_point(nav_msgs::OccupancyGrid grid) {
+    thread_local static mt19937 rng(std::random_device{}());
+    thread_local static uniform_real_distribution<double> dist(0.0, 1.0);
+        
+    double mapWidth = grid.info.resolution * grid.info.width;
+    double mapHeight = grid.info.resolution * grid.info.height;
+    
+    return coord {
+        dist(rng) * mapWidth - grid.info.origin.position.x, 
+        dist(rng) * mapHeight - grid.info.origin.position.y
+    };
 }
 
-indices get_indices(coords c, gazebo_msgs::OccupancyGrid grid) {
-    return {
-		(int)(get<0>(c) / grid.info.resolution), 
-		(int)(get<1>(c) / grid.info.resolution)
-	};
+indices get_indices(coord c, nav_msgs::OccupancyGrid grid) {
+    return indices {
+        (int)(get<0>(c) / grid.info.resolution), 
+        (int)(get<1>(c) / grid.info.resolution)
+    };
 }
 
-coords get_coordinates(indices i) {
-    return {
-		(0.5 + get<0>(i)) * grid.info.resolution, 
-		(0.5 + get<1>(i)) * grid.info.resolution 
-	};
+bool is_connection_valid(coord start, coord finish, nav_msgs::OccupancyGrid grid) {
+    return true;
+    // auto idx1 = bresenham(get_indices(start, grid))
 }
 
-int get_og_index(indices idx) {
-	auto x = get<0>(idx);
-	auto y = get<1>(idx);
-    return (x + cell_width / 2) + (y + cell_height / 2) * cell_width;
+
+coord get_coordinates(indices i, nav_msgs::OccupancyGrid grid) {
+    return coord {
+        (0.5 + get<0>(i)) * grid.info.resolution, 
+        (0.5 + get<1>(i)) * grid.info.resolution 
+    };
 }
 
-boolean is_milestone_valid(coord ms, gazebo_msgs::OccupancyGrid grid) {
-	auto idx = get_og_index(get_indices(ms));
-	return grid[idx] > 10;
+int get_og_index(indices idx, nav_msgs::OccupancyGrid grid) {
+    auto x = get<0>(idx);
+    auto y = get<1>(idx);
+    return (x + grid.info.width / 2) + (y + grid.info.height / 2) * grid.info.width;
+}
+
+bool is_milestone_valid(coord ms, nav_msgs::OccupancyGrid grid) {
+    auto idx = get_og_index(get_indices(ms, grid), grid);
+    return grid.data[idx] > 10;
 }
 
 vector<int> a_star(int start, int end, vector<coord> milestones, edgeset edges) {
-	auto endpoint = milestone[end];
-	
-	// Open and closed sets
-	set<int> open;
-	set<int> closed; 
-	
-	// Bookkeeping
-	map<int, int> trajectory;
-	map<int, double> cost_actual;
-	map<int, double> cost_lower_bound;
-	
-	// Seed open set with start node
-	open.insert(start);
-	cost_actual[start] = 0;
-	cost_lower_bound[start] = cost_actual[start] + get_distance(milestones[start], endpoint);
-	
-	// While the best node isn't the goal
-	while (!open.empty()) {
-		int current;
-		double lowest = numeric_limits<double>::infinity();
-		
-		// Choose the node from the open set with the lowest lower-bound estimate of cost
-		for (auto i: open) {
-			if (cost_lower_bound[i] < lowest) {
-				lowest = cost_lower_bound[i];
-				current = i;
-			}
-		}
-		
-		// If we've already reached the goal, walk backwards along the trajectory 
-		// to construct a vector of node indices and return it
-		if (current == end) {
-			vector<int> result;
-			result.push_back(end);
-			
-			while (trajectory.count(result.back())) {
-				result.push_back(trajectory[result.back()]);
-			}
-			
-			return result;
-		}
-		
-		// Remove current from open set and add to closed set
-		open.remove(current);
-		closed.insert(current);
-		
-		// Deal with neighbors of current cell
-		for (auto kv: edges[current]) {
-			auto neighbor = kv.first;
-			auto distance = kv.second;
-			
-			// Ignore any neighbors which have already been evaluated
-			if (closed.count(neighbor)) {
-				continue;
-			}
-			
-			// Insert the neighbor in the open set and work out total cost from start node
-			open.insert(neighbor);
-			auto cost = cost_actual[current] + distance;
-			
-			// If this is a newly visited node or previous visits were through suboptimal path,
-			// update metrics
-			if (!trajectory.count(neighbor) || cost < cost_actual[neighbor]) {
-				trajectory[neighbor] = current;
-				cost_actual[neighbor] = cost;
-				cost_lower_bound[neighbor] = cost + get_distance(milestones[neighbor], endpoint);
-			}
-		}
-	}
-	
-	// If we haven't returned by this point, there is no path
-	return {};
+    auto endpoint = milestones[end];
+    
+    // Open and closed sets
+    set<int> open;
+    set<int> closed; 
+    
+    // Bookkeeping
+    map<int, int> trajectory;
+    map<int, double> cost_actual;
+    map<int, double> cost_lower_bound;
+    
+    // Seed open set with start node
+    open.insert(start);
+    cost_actual[start] = 0;
+    cost_lower_bound[start] = cost_actual[start] + get_distance(milestones[start], endpoint);
+    
+    // While the best node isn't the goal
+    while (!open.empty()) {
+        int current;
+        double lowest = numeric_limits<double>::infinity();
+        
+        // Choose the node from the open set with the lowest lower-bound estimate of cost
+        for (auto i: open) {
+            if (cost_lower_bound[i] < lowest) {
+                lowest = cost_lower_bound[i];
+                current = i;
+            }
+        }
+        
+        // If we've already reached the goal, walk backwards along the trajectory 
+        // to construct a vector of node indices and return it
+        if (current == end) {
+            vector<int> result;
+            result.push_back(end);
+            
+            while (trajectory.count(result.back())) {
+                result.push_back(trajectory[result.back()]);
+            }
+            
+            return result;
+        }
+        
+        // Remove current from open set and add to closed set
+        open.erase(current);
+        closed.insert(current);
+        
+        // Deal with neighbors of current cell
+        for (auto kv: edges[current]) {
+            auto neighbor = kv.first;
+            auto distance = kv.second;
+            
+            // Ignore any neighbors which have already been evaluated
+            if (closed.count(neighbor)) {
+                continue;
+            }
+            
+            // Insert the neighbor in the open set and work out total cost from start node
+            open.insert(neighbor);
+            auto cost = cost_actual[current] + distance;
+            
+            // If this is a newly visited node or previous visits were through suboptimal path,
+            // update metrics
+            if (!trajectory.count(neighbor) || cost < cost_actual[neighbor]) {
+                trajectory[neighbor] = current;
+                cost_actual[neighbor] = cost;
+                cost_lower_bound[neighbor] = cost + get_distance(milestones[neighbor], endpoint);
+            }
+        }
+    }
+    
+    // If we haven't returned by this point, there is no path
+    return {};
 }
 
-tuple<vector<coord>, edgeset> prm(int n, int k, coord start, vector<coord> targets, gazebo_msgs::OccupancyGrid grid) {	
-	// Figure out map width and height
-	double mapWidth = grid.info.resolution * grid.info.width;
-	double mapHeight = grid.info.resolution * grid.info.height;
-	
-	// Start generating graph
-	vector<coord> milestones;
-	edgeset edges;
-	
-	// Seed graph with start node and targets
-	milestones.push_back(start);
-	milestones.insert(milestones.end(), targets.start(), targets.end());
-	
-	// Sample a collision-free set of milestones
-	while (milestones.size() < n) {
-		coords point;
-		// Sample a new point
-		do {
-			point = sample_point()
-		} while (!is_milestone_valid(point))
-		
-		milestones.push_back(point);
-	}
-	
-	// For each milestone, connect all legal paths
-	for (auto i: milestones) {
-		for (auto j: find_nearest_neighbors(i, milestones, k)) {
-			if (is_connection_valid(milestones[i], milestones[j], grid)) {
-				edges[i][j] = get_distance(milestones[i], milestones[j]);
-			}
-		}
-	}
-	
-	return {milestones, edges};
+tuple<vector<coord>, edgeset> prm(const int n, const int k, coord start, vector<coord> targets, nav_msgs::OccupancyGrid grid) {    
+    // Figure out map width and height
+    double mapWidth = grid.info.resolution * grid.info.width;
+    double mapHeight = grid.info.resolution * grid.info.height;
+    
+    // Start generating graph
+    vector<coord> milestones;
+    edgeset edges;
+    
+    // Seed graph with start node and targets
+    milestones.push_back(start);
+    milestones.insert(milestones.end(), targets.begin(), targets.end());
+    
+    // Sample a collision-free set of milestones
+    while (milestones.size() < n) {
+        coord point;
+        // Sample a new point
+        do {
+            point = sample_point(grid);
+        } while (!is_milestone_valid(point, grid));
+        
+        milestones.push_back(point);
+    }
+    
+    // For each milestone, connect all legal paths
+    for (auto it = milestones.begin(); it != milestones.end(); it++) {
+	int i = distance(milestones.begin(), it);
+        for (auto j: find_nearest_neighbors(milestones[i], milestones, k)) {
+            if (is_connection_valid(milestones[i], milestones[j], grid)) {
+                edges[i][j] = get_distance(milestones[i], milestones[j]);
+            }
+        }
+    }
+    
+    return tuple<vector<coord>, edgeset>{milestones, edges};
 }
