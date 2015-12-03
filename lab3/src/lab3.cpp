8a97b6c3552ea6e2ed786402cf58b9b127528582
Dan Shaw <d3shaw@gmail.com>
2015-12-03 03:08:46 -0500

    visualization implemented, a_star path, and IPS logging

--- lab3/src/lab3.cpp
+++ lab3/src/lab3.cpp
@@ -28,6 +28,7 @@ typedef tuple<double, double> coord;
 #define TAGID 0
 
 ros::Publisher marker_pub;
+ros::Publisher marker_RobotPoints;
 Eigen::Vector3d IPS;
 const int m = 100;
 const int k = 10;
@@ -117,15 +118,57 @@ void drawPoint(int k, geometry_msgs::Point p)
 	points.pose.orientation.w = 1.0;
 	points.id = k;
 	points.type = visualization_msgs::Marker::POINTS;
-	points.scale.x = 0.2;
-	points.scale.y = 0.2;
+	points.scale.x = 0.05;
+	points.scale.y = 0.05;
 	points.color.b = 1.0f;
 	points.color.a = 1.0;
-	points.points.push_back(p); //just changed this
-
+	points.points.push_back(p); 
 	usleep(2000);
 	//std::cin.get();
 	marker_pub.publish(points);
+}
+
+void drawWaypoint(int k, geometry_msgs::Point p) 
+{
+	visualization_msgs::Marker points; //Define points
+	points.header.frame_id = "/map";
+	points.header.stamp = ros::Time::now();
+	points.ns = "lab3";
+	points.action = visualization_msgs::Marker::ADD;
+	points.pose.orientation.w = 1.0;
+	points.id = k;
+	points.type = visualization_msgs::Marker::POINTS;
+	points.scale.x = .2;
+	points.scale.y = .2;
+	points.color.b = 0.0f;
+	points.color.g = 1.0f;
+	points.color.a = 1.0;
+	points.points.push_back(p); 
+	usleep(2000);
+	//std::cin.get();
+	marker_pub.publish(points);
+}
+
+void drawRobotPoint(int k, geometry_msgs::Point p) 
+{
+	visualization_msgs::Marker points; //Define points
+	points.header.frame_id = "/map";
+	points.header.stamp = ros::Time::now();
+	points.ns = "lab3";
+	points.action = visualization_msgs::Marker::ADD;
+	points.pose.orientation.w = 1.0;
+	points.id = k;
+	points.type = visualization_msgs::Marker::POINTS;
+	points.scale.x = .05;
+	points.scale.y = .05;
+	points.color.b = 0.39f;
+	points.color.g = 0.90f;
+	points.color.r = 0.87f;
+	points.color.a = 1.0;
+	points.points.push_back(p); 
+	usleep(2000);
+	//std::cin.get();
+	marker_RobotPoints.publish(points);
 }
 
 void drawLineSegment(int k, geometry_msgs::Point start_point, geometry_msgs::Point end_point)
@@ -140,9 +183,37 @@ void drawLineSegment(int k, geometry_msgs::Point start_point, geometry_msgs::Poi
 	lines.type = visualization_msgs::Marker::LINE_STRIP;
 	lines.action = visualization_msgs::Marker::ADD;
 	
-	lines.scale.x = 0.1;
+	lines.scale.x = 0.02;
+	lines.scale.y = 0.02;
 	lines.color.r = 1.0;
 	lines.color.b = 0.2;
+	lines.color.a = 0.5;
+
+	lines.points.push_back(start_point);
+	lines.points.push_back(end_point);
+
+	//publish new segment line
+	usleep(2000);
+	marker_pub.publish(lines);
+	//sleep(0.05);
+}
+
+void drawWHEYPROTEIN(int k, geometry_msgs::Point start_point, geometry_msgs::Point end_point)
+{
+	visualization_msgs::Marker lines;
+	lines.header.frame_id = "/map";
+	lines.type = visualization_msgs::Marker::LINE_STRIP;
+	lines.ns = "lab3";
+	
+	
+	lines.id = k; //each curve must have a unique id or you will overwrite an old ones
+	lines.type = visualization_msgs::Marker::LINE_STRIP;
+	lines.action = visualization_msgs::Marker::ADD;
+	
+	lines.scale.x = 0.05;
+	lines.scale.y = 0.05;
+	lines.color.r = 1.0;
+	lines.color.b = 1.0;
 	lines.color.a = 1.0;
 
 	lines.points.push_back(start_point);
@@ -165,6 +236,7 @@ void delLine(int k)
 	//publish new segment line
 	usleep(2000);
 	marker_pub.publish(lines);
+	//marker_RobotPoints.publish(lines);
 }
 
 void drawPrm(tuple<vector<coord>, map<int, map<int, double>>> prmResult) {
@@ -174,7 +246,7 @@ void drawPrm(tuple<vector<coord>, map<int, map<int, double>>> prmResult) {
 	int num_nodes=coords.size();
 	std::cout<<num_nodes<<std::endl;
 
-	for (int i=0; i<m*k; i++)
+	for (int i=0; i<m*(k+1); i++)
 	{
 		usleep(2000);
 		delLine(i);
@@ -224,7 +296,65 @@ void drawPrm(tuple<vector<coord>, map<int, map<int, double>>> prmResult) {
         		drawLineSegment(pointId++, start_, end_);
 		
 		}
-	} 
+	}
+}
+
+
+void drawChosenPath(vector<int> path_nodes,tuple<vector<coord>, map<int, map<int, double>>> prmResult) {
+	// Plot the points
+	int pointId = 0;
+	int num_nodes = path_nodes.size();
+	double x = IPS[0];
+	double y = IPS[1];
+
+	geometry_msgs::Point p;
+	p.x = 1.45;
+	p.y = -2.23+1.5;
+	p.z = 0.1;
+		
+	usleep(2000);
+	drawWaypoint(pointId++, p);
+	p.x = 0.08;
+	p.y = 0.6+1.5;
+	p.z =0.1;
+		
+	usleep(2000);
+	drawWaypoint(pointId++, p);
+	p.x = -1.72;
+	p.y = 0.43+1.5;
+	p.z = 0.1;
+		
+	usleep(2000);
+	drawWaypoint(pointId++, p);
+	//std::cin.get();
+
+	vector<coord> coords = std::get<0>(prmResult);
+	for (int i=0; i<num_nodes; i++)
+	{
+		geometry_msgs::Point start_, end_;
+		coord item = coords[path_nodes[i]];
+		cout << "This is pathnodes " << path_nodes[i] << endl;
+		
+		cout << "pathnode x: " << std::get<0>(item);
+	 	cout << " pathnode y: " << std::get<1>(item) << endl;
+		
+
+		start_.x = x;
+       		start_.y = y;
+		start_.z = 0.2;    
+       		end_.x = std::get<0>(item);
+       		end_.y = std::get<1>(item);
+		end_.z = 0.2;
+		usleep(2000);
+		drawWHEYPROTEIN(pointId++, start_, end_);
+		
+		x=std::get<0>(item);
+		y=std::get<1>(item);
+		cout << x << endl;
+		cout << y << endl;
+	}
+
+
 }
 
 
@@ -239,6 +369,7 @@ int main(int argc, char** argv)
     auto pose_sub_sim = n.subscribe("/gazebo/model_states", 1, pose_callback_sim);
     auto velocity_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
     marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 20);
+	marker_RobotPoints = n.advertise<visualization_msgs::Marker>("Robot_marker", 20);
     // Velocity control variable
     geometry_msgs::Twist vel;
 
@@ -262,6 +393,7 @@ int main(int argc, char** argv)
     // Get the probabilistic roadmap
     bool success = true;
     int attempt = 0;
+	int RobotPosCount =0;
     tuple<vector<coord>, map<int, map<int, double> > > result;
     vector<int> path_nodes;
 
@@ -282,6 +414,9 @@ int main(int argc, char** argv)
             }
         }
     } while (!success);
+    
+    drawChosenPath(path_nodes,result);
+	
     
     // Preparing for tracking
     ROS_INFO("Preparing tracking");
@@ -335,5 +470,11 @@ int main(int argc, char** argv)
         cout << "Outside X: " << outsideXThreshold
                 << " Outside Y: " << outsideYThreshold << endl;
         velocity_publisher.publish(vel);
+
+	geometry_msgs::Point p;
+	p.x = IPS[0];
+	p.y = IPS[1];
+	p.z = 0.4;
+	drawRobotPoint(	RobotPosCount++, p);
     }
 }
